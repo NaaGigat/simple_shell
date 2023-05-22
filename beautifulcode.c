bd91fc0101504a41779c83d1f048c2992af0d072
sharpartzgh <sharpartzprojects@yahoo.com>
2023-05-22 08:52:37 -0700

    passes aunty betty checks

---
+++ beautifulcode.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * main - entry
+ * print - string
+ * return: 0(success)
+ *
+ */
+
+int main(void);
+{
+	printf("Fred and Naa, Shell project!\n");
+	return (0);
+}
